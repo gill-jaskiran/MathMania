eb39da87655413cb052e016a309b6c9fcc7fc3ce
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2025-02-17 11:28:12 -0500

    Initial commit

---
+++ MathManiaApp.swift
@@ -0,0 +1,16 @@
+//
+//  MathManiaApp.swift
+//  MathMania
+//
+//
+
+import SwiftUI
+
+@main
+struct MathManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
