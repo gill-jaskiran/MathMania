297161c7898888a7ee57ff9cac04054d794af758
eliza2526 <elizabeth.thomas2@georgebrown.ca>
2025-02-17 19:27:26 -0500

    Leaderboard Class First Commit

--- Leaderboard.swift
+++ Leaderboard.swift
@@ -2,6 +2,20 @@
 //  Leaderboard.swift
 //  MathMania
 //
+//  Created by Elizabeth Thomas on 2025-02-17.
 //
+import Foundation
 
-
+class Leaderboard: ObservableObject {
+    @Published var items: [LeaderboardItem] = []
+    
+    init(items: [LeaderboardItem] = []) {
+        self.items = items
+    }    
+          
+    func addScore(username: String, score: Int) {
+        let newItem = LeaderboardItem(username: username, score: score)
+        leaderboard.append(newItem)
+        leaderboard.sort { $0.score>$1.score }    
+    }
+}
