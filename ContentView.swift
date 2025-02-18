025b204f02fd022cffed6fdcb3005184ff739dd5
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2025-02-18 00:46:02 -0500

    Updating ContentView - Adding UI

--- ContentView.swift
+++ ContentView.swift
@@ -23,15 +23,23 @@ struct ContentView: View {
                     .padding()
 
                 TextField("Enter your name", text: $username)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
 
                 Button(action: {
                     leaderboard.addScore(username: username.isEmpty ? "Anonymous" : username, score: gameManager.score)
                 }) {
                     Text("Submit Score")
-                        
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
                 }
+                .padding()
 
                 Text("Top 7 Leaderboard:")
+                    .font(.title2)
+                    .padding()
 
                 List(leaderboard.leaderboard) { item in
                     HStack {
@@ -46,23 +54,47 @@ struct ContentView: View {
                     username = ""
                 }) {
                     Text("Play Again")
-                     
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
                 }
+                .padding()
+
             } else {
                 Text("Level \(gameManager.currentLevel)")
+                    .font(.largeTitle)
+                    .padding()
+
                 Text(gameManager.question)
+                    .font(.title2)
+                    .padding()
 
                 if gameManager.currentLevel == 1 {
                     HStack {
                         Button(action: { gameManager.checkGuess(guess: "Odd") }) {
                             Text("Odd")
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
                         }
+                        .padding()
+
                         Button(action: { gameManager.checkGuess(guess: "Even") }) {
+                            Text("Even")
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
                         }
                         .padding()
                     }
                 } else {
                     TextField("Enter your answer", text: $username)
+                        .padding()
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
 
                     Button(action: {
                         if let answer = Int(username) {
@@ -71,13 +103,20 @@ struct ContentView: View {
                         }
                     }) {
                         Text("Submit Answer")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
                     }
-
+                    .padding()
                 }
 
                 Text("Score: \(gameManager.score)")
+                    .font(.title)
+                    .padding()
             }
         }
+        .padding()
     }
 }
 
