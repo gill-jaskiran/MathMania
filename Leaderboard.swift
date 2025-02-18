2e8508c5038998d737c0832f1e964b8a3d53bf4b
eliza2526 <elizabeth.thomas2@georgebrown.ca>
2025-02-17 19:58:03 -0500

    Leaderboard.swift - Final Commit

--- Leaderboard.swift
+++ Leaderboard.swift
@@ -6,16 +6,44 @@
 //
 import Foundation
 
+struct LeaderboardItem: Codable, Identifiable {
+    var id = UUID()
+    var username: String
+    var score: Int
+}
+
 class Leaderboard: ObservableObject {
-    @Published var items: [LeaderboardItem] = []
+    @Published var leaderboard: [LeaderboardItem] = []
     
-    init(items: [LeaderboardItem] = []) {
-        self.items = items
-    }    
-          
+    init() {
+        loadLeaderboard()
+    }
+    
+    func loadLeaderboard() {
+        if let savedData = UserDefaults.standard.data(forKey: "leaderboard") {
+            let decoder = JSONDecoder()
+            if let decodedData = try? decoder.decode([LeaderboardItem].self, from: savedData) {
+                leaderboard = decodedData
+            }
+        }
+    }
+    
+    func saveLeaderboard() {
+        let encoder = JSONEncoder()
+        if let encodedData = try? encoder.encode(leaderboard) {
+            UserDefaults.standard.set(encodedData, forKey: "leaderboard")
+        }
+    }
+        
     func addScore(username: String, score: Int) {
         let newItem = LeaderboardItem(username: username, score: score)
         leaderboard.append(newItem)
-        leaderboard.sort { $0.score>$1.score }    
+        leaderboard.sort { $0.score>$1.score }
+        
+        if leaderboard.count > 7 {
+            leaderboard = Array(leaderboard.prefix(7))
+        }
+
+    saveLeaderboard()
     }
 }
