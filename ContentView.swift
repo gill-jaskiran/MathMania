0c822b21b5b31fa2a78e155ea46aacaec3ef089a
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2025-03-29 11:48:33 -0400

    Updating content view with navigation

--- ContentView.swift
+++ ContentView.swift
@@ -2,165 +2,31 @@
 //  ContentView.swift
 //  MathMania
 //
-//  Created by Elizabeth Thomas on 2025-03-20.
+//  Created by Jaskiran Gill on 2025-03-29.
 //
 import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var gameManager = GameManager()
     @ObservedObject var leaderboard = Leaderboard()
-    @State private var username: String = ""
-
+    
     var body: some View {
-        VStack {
-            if gameManager.gameOver {
-                Text("Game Over!")
-                    .font(.largeTitle)
-                    .padding()
-
-                Text("Final Score: \(gameManager.score)")
-                    .font(.title)
-                    .padding()
-
-                /* TextField("Enter your name", text: $username)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle()) */
-
-                Button(action: {
-                    leaderboard.addScore(score: gameManager.score)
-                }) {
-                    Text("Submit Score")
-                        .padding()
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+        NavigationStack {
+            VStack {
+                if gameManager.gameOver {
+                    GameOverView(gameManager: gameManager, leaderboard: leaderboard)
+                } else {
+                    LevelView(gameManager: gameManager)
                 }
-                .padding()
-
-                Text("Recent Scores: ")
-                    .font(.title2)
-                    .padding()
-
-                List(leaderboard.leaderboard) { item in
-                    HStack {
-                        Text(item.username)
-                        Spacer()
-                        Text("\(item.score) points")
-                    }
-                }
-
-                Button(action: {
-                    gameManager.resetGame()
-                    username = ""
-                }) {
-                    Text("Play Again")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding()
-
-            } else {
-                Text("Level \(gameManager.currentLevel)")
-                    .font(.largeTitle)
-                    .padding()
-
-                Text(gameManager.question)
-                    .font(.title2)
-                    .padding()
-
-                if gameManager.currentLevel == 1 {
-                    HStack {
-                        Button(action: { gameManager.checkGuess(guess: "Odd") }) {
-                            Text("Odd")
-                                .padding()
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
-                        .padding()
-
-                        Button(action: { gameManager.checkGuess(guess: "Even") }) {
-                            Text("Even")
-                                .padding()
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
-                        .padding()
-                    }
-                } else if gameManager.currentLevel == 2 {
-                    TextField("Enter your answer", text: $username)
-                        .padding()
-                        .keyboardType(.numberPad)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-
-                    Button(action: {
-                        if let answer = Int(username) {
-                            gameManager.checkAnswer(answer: answer)
-                            username = ""
-                        }
-                    }) {
-                        Text("Submit Answer")
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                    .padding()
-                } else if gameManager.currentLevel == 3 {
-                    VStack {
-                        Button(action: { gameManager.checkPrimeGuess(guess: "Yes Prime") }) {
-                            HStack {
-                                Image(systemName: "checkmark.seal.fill")
-                                    .foregroundColor(.white)
-                                Text("Prime")
-                                    .fontWeight(.bold)
-                            }
-                            .padding()
-                            .frame(width: 180, height: 60)
-                            .background(
-                                LinearGradient(gradient: Gradient(colors: [Color.purple, Color.pink]), startPoint: .leading, endPoint: .trailing)
-                            )
-                            .foregroundColor(.white)
-                            .cornerRadius(15)
-                            .shadow(radius: 5)
-                        }
-                        .padding()
-
-                        Button(action: { gameManager.checkPrimeGuess(guess: "Not Prime") }) {
-                            HStack {
-                                Image(systemName: "xmark.octagon.fill")
-                                    .foregroundColor(.white)
-                                Text("Not Prime")
-                                    .fontWeight(.bold)
-                            }
-                            .padding()
-                            .frame(width: 180, height: 60)
-                            .background(
-                                LinearGradient(gradient: Gradient(colors: [Color.pink, Color.orange]), startPoint: .leading, endPoint: .trailing)
-                            )
-                            .foregroundColor(.white)
-                            .cornerRadius(15)
-                            .shadow(radius: 5)
-                        }
-                        .padding()
-                    }
-                    .padding(.top, 20)
-                }
-
-                Text("Score: \(gameManager.score)")
-                    .font(.title)
-                    .padding()
             }
+            .navigationTitle("Math Mania")
+            .padding()
         }
-        .padding()
     }
 }
 
-
-#Preview {
-    ContentView()
-
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
